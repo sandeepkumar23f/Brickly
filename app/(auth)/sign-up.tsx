44c87d7bbd73532f7d5ecf579c0a87698dafe3a5
sandeepkumar23f <sandeep21000552@gmail.com>
2026-06-03 10:44:20 +0530

    added the login and signup page

--- app/(auth)/sign-up.tsx
+++ app/(auth)/sign-up.tsx
@@ -1,10 +1,10 @@
 import { View, Text } from 'react-native'
 import React from 'react'
 
-export default function SignIn() {
+export default function SignUp() {
   return (
     <View className="flex-1 items-center justify-center bg-white">
-      <Text className="text-xl font-bold">SignIn</Text>
+      <Text className="text-xl font-bold">SignUp</Text>
     </View>
   )
 }
