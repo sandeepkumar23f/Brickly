01ffab2c1853f58ad42e9ff535261bca5787430e
sandeepkumar23f <sandeep21000552@gmail.com>
2026-06-03 10:41:16 +0530

    clerk setup done.

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,10 +1,16 @@
-import { Stack } from 'expo-router';
-import { SafeAreaProvider } from 'react-native-safe-area-context';
+import { ClerkProvider } from '@clerk/expo'
+import { tokenCache } from '@clerk/expo/token-cache'
+import { Slot } from 'expo-router'
+const publishableKey = process.env.EXPO_PUBLIC_CLERK_PUBLISHABLE_KEY!
+
+if (!publishableKey) {
+  throw new Error('Add your Clerk Publishable Key to the .env file')
+}
 
 export default function RootLayout() {
   return (
-    <SafeAreaProvider>
-      <Stack screenOptions={{ headerShown: false }} />
-    </SafeAreaProvider>
-  );
+    <ClerkProvider publishableKey={publishableKey} tokenCache={tokenCache}>
+      <Slot />
+    </ClerkProvider>
+  )
 }
--- app/index.tsx
+++ app/index.tsx
@@ -1,5 +1,11 @@
+import { useAuth } from "@clerk/expo";
 import { Redirect } from "expo-router";
 
 export default function Index(){
-    return <Redirect href="/(root)/(tabs)" />
+    const { isSignedIn, isLoaded } = useAuth()
+
+    if(!isLoaded) return null;
+    // redirect based on auth state
+    if(isSignedIn)  return <Redirect href="/(root)/(tabs)" />
+    return <Redirect href="/sign-in" />
 }
