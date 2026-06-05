053665f79414608969596b61f8c1aa0e63caa5c3
sandeepkumar23f <sandeep21000552@gmail.com>
2026-06-05 07:13:36 +0530

    fixed the issues arrises during tailwind setup and now tailwind is proper working

--- app/(root)/(tabs)/search.tsx
+++ app/(root)/(tabs)/search.tsx
@@ -1,12 +1,11 @@
 import { View, Text } from 'react-native'
 import React from 'react'
 import { SafeAreaView } from 'react-native-safe-area-context'
-
 export default function search() {
   return (
     <SafeAreaView>
       <View>
-        <Text>Search</Text>
+        <Text className='text-xl font-extrabold text-blue-500'>Search</Text>
       </View>
     </SafeAreaView>
   )
--- app/index.tsx
+++ app/index.tsx
@@ -1,6 +1,6 @@
 import { useAuth } from "@clerk/expo";
 import { Redirect } from "expo-router";
-
+import "../global.css"
 export default function Index(){
     const { isSignedIn, isLoaded } = useAuth()
 
