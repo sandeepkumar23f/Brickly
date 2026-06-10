38d0a8106bf2256bbac8c9cb459db611d89974b6
sandeepkumar23f <sandeep21000552@gmail.com>
2026-06-10 06:41:18 +0530

    removed the clerk totally

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,20 +1,15 @@
-import { ClerkProvider } from "@clerk/expo";
-import { useAuth } from "@clerk/expo";
+
 
 import { Redirect, Slot } from "expo-router";
 
 export default function RootLayout() {
   return (
-    <ClerkProvider publishableKey="YOUR_CLERK_PUBLISHABLE_KEY">
-      <AuthGate />
-    </ClerkProvider>
+        <AuthGate />
+    
   );
 }
 
 function AuthGate() {
-  const { isSignedIn, isLoaded } = useAuth();
 
-  if (!isLoaded) return null;
-  if (!isSignedIn) return <Redirect href="/sign-in" />;
-  return <Slot />;
+  return <Redirect href="/sign-in" />;
 }
